ff533551320da3fa45f55bb2fcd238fbf55319ee
creightontaylor <creightontaylor@gmail.com>
2024-06-04 09:04:26 -0700

    feat: Implement caching mechanism using shelve for API data generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -1,6 +1,7 @@
 import os
 import requests
-
+import shelve
+import time
 # Securely access GitHub Personal Access Token from environment variables
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 GITHUB_REPO = 'https://api.github.com/repos/username/repository'
@@ -12,12 +13,24 @@ headers = {
 }
 
 # Fetch the number of commits
-commits_response = requests.get(f'{GITHUB_REPO}/commits', headers=headers)
-commits_data = commits_response.json()
+cache_key = 'commits_data'
+with shelve.open('api_cache') as cache:
+    if cache_key in cache and time.time() - cache[cache_key]['timestamp'] < 86400:
+        commits_data = cache[cache_key]['data']
+    else:
+        commits_response = requests.get(f'{GITHUB_REPO}/commits', headers=headers)
+        commits_data = commits_response.json()
+        cache[cache_key] = {'data': commits_data, 'timestamp': time.time()}
 
 # Fetch the number of open and closed issues
-issues_response = requests.get(f'{GITHUB_REPO}/issues', headers=headers, params={'state': 'all'})
-issues_data = issues_response.json()
+cache_key = 'issues_data'
+with shelve.open('api_cache') as cache:
+    if cache_key in cache and time.time() - cache[cache_key]['timestamp'] < 86400:
+        issues_data = cache[cache_key]['data']
+    else:
+        issues_response = requests.get(f'{GITHUB_REPO}/issues', headers=headers, params={'state': 'all'})
+        issues_data = issues_response.json()
+        cache[cache_key] = {'data': issues_data, 'timestamp': time.time()}
 
 open_issues = len([issue for issue in issues_data if issue['state'] == 'open'])
 closed_issues = len(issues_data) - open_issues
